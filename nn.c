528b1186bf6ed2d9bbc7bc654f6b8b3e0bb6b335
Mahmoud Elkholy <midoelkholy03@gmail.com>
2026-05-18 03:00:34 +0300

    apply the matrix pardigm to implemnt the xor gate

--- nn.c
+++ nn.c
@@ -1,32 +1,179 @@
 #include <stdio.h>
 #include <time.h>
+#include <assert.h>
 
 #define NN_IMPLEMENTATION
 #include "nn.h"
 
+typedef struct {
+    Mat a0;
+    Mat w1, b1, a1;
+    Mat w2, b2, a2;
+} Xor;
+
+Xor xor_init() {
+    Xor m;
+    m.a0 = mat_alloc(1, 2);
+    m.w1 = mat_alloc(2, 2);
+    m.b1 = mat_alloc(1, 2);
+    m.a1 = mat_alloc(1, 2);
+    m.w2 = mat_alloc(2, 1);
+    m.b2 = mat_alloc(1, 1);
+    m.a2 = mat_alloc(1, 1);
+
+    mat_rand(m.w1, 0, 1);
+    mat_rand(m.b1, 0, 1);
+    mat_rand(m.w2, 0, 1);
+    mat_rand(m.b2, 0, 1);
+
+    return m;
+}
+
+void xor_forward(Xor m) {
+
+    mat_dot(m.a1, m.a0, m.w1);
+    mat_sum(m.a1,  m.b1);
+    mat_sig(m.a1);
+
+    mat_dot(m.a2, m.a1, m.w2);
+    mat_sum(m.a2, m.b2);
+    mat_sig(m.a2);
+}
+
+float xor_cost(Xor m, Mat ti, Mat to) {
+    assert(ti.rows == to.rows);
+    assert(to.cols == m.a2.cols);
+
+    size_t n = ti.rows;
+    size_t q = to.cols;
+    float err = 0;
+
+    for (size_t i = 0; i < n; i++) {
+        Mat x = mat_row(ti, i);
+        Mat y = mat_row(to, i);
+        mat_copy(m.a0, x);
+        xor_forward(m);
+        for (size_t j = 0; j < q; j++) {
+            float diff =  MAT_AT(m.a2, 0, j) - MAT_AT(y, 0, j);
+            err += diff*diff;
+        }
+    }
+
+    return err/n;
+}
+
+void xor_finite_diff(Xor m, Xor g, Mat ti, Mat to, float eps) {
+    float cost = xor_cost(m, ti, to);
+
+    for (size_t i = 0; i < m.w1.rows; i++) {
+        for (size_t j = 0; j < m.w1.cols; j++) {
+            float saved = MAT_AT(m.w1, i, j);
+            MAT_AT(m.w1, i, j) += eps;
+            MAT_AT(g.w1, i, j) = (xor_cost(m, ti, to) - cost) / eps;
+            MAT_AT(m.w1, i, j) = saved;
+        }
+    }
+
+    for (size_t i = 0; i < m.w2.rows; i++) {
+        for (size_t j = 0; j < m.w2.cols; j++) {
+            float saved = MAT_AT(m.w2, i, j);
+            MAT_AT(m.w2, i, j) += eps;
+            MAT_AT(g.w2, i, j) = (xor_cost(m, ti, to) - cost) / eps;
+            MAT_AT(m.w2, i, j) = saved;
+        }
+    }
+
+    for (size_t i = 0; i < m.b1.rows; i++) {
+        for (size_t j = 0; j < m.b1.cols; j++) {
+            float saved = MAT_AT(m.b1, i, j);
+            MAT_AT(m.b1, i, j) += eps;
+            MAT_AT(g.b1, i, j) = (xor_cost(m, ti, to) - cost) / eps;
+            MAT_AT(m.b1, i, j) = saved;
+        }
+    }
+
+    for (size_t i = 0; i < m.b2.rows; i++) {
+        for (size_t j = 0; j < m.b2.cols; j++) {
+            float saved = MAT_AT(m.b2, i, j);
+            MAT_AT(m.b2, i, j) += eps;
+            MAT_AT(g.b2, i, j) = (xor_cost(m, ti, to) - cost) / eps;
+            MAT_AT(m.b2, i, j) = saved;
+        }
+    }
+}
+
+void xor_learn(Xor m, Xor g, float rate) {
+    for (size_t i = 0; i < m.w1.rows; i++) {
+        for (size_t j = 0; j < m.w1.cols; j++) {
+            MAT_AT(m.w1, i, j) -= rate*MAT_AT(g.w1, i, j);
+        }
+    }
+
+    for (size_t i = 0; i < m.w2.rows; i++) {
+        for (size_t j = 0; j < m.w2.cols; j++) {
+            MAT_AT(m.w2, i, j) -= rate*MAT_AT(g.w2, i, j);
+        }
+    }
+
+    for (size_t i = 0; i < m.b1.rows; i++) {
+        for (size_t j = 0; j < m.b1.cols; j++) {
+            MAT_AT(m.b1, i, j) -= rate*MAT_AT(g.b1, i, j);
+        }
+    }
+
+    for (size_t i = 0; i < m.b2.rows; i++) {
+        for (size_t j = 0; j < m.b2.cols; j++) {
+            MAT_AT(m.b2, i, j) -= rate*MAT_AT(g.b2, i, j);
+        }
+    }
+}
+
+void xor_truth_taple(Xor m) {
+    for (size_t i = 0; i < 2; i++) {
+        for (size_t j = 0; j < 2; j++) {
+            MAT_AT(m.a0, 0, 0) = i;
+            MAT_AT(m.a0, 0, 1) = j;
+            xor_forward(m);
+            float y = *m.a2.es;
+            printf("%zu ^ %zu = %f\n", i, j , y);
+        }
+    }
+}
+
+float td[] = {
+    0, 0, 0,
+    0, 1, 1,
+    1, 0, 1,
+    1, 1, 0,
+};
+
 int main() {
     srand(time(0));
-    float unit_arr[] = {
-        1 ,0,
-        0 ,1,
-    };
 
-    Mat b = mat_alloc(2, 2);
-    mat_rand(b, 0, 10);
+    float eps  = 1e-1;
+    float rate = 1e-1;
 
-    Mat unit_mat = {
-        .rows = 2,
+    Xor m = xor_init();
+    Mat ti = {
+        .rows = 4,
         .cols = 2,
-        .data = unit_arr,
+        .stride = 3,
+        .es = td,
     };
 
-    MAT_PRINT(b);
-    MAT_PRINT(unit_mat);
-    printf("----------------------\n");
-    Mat dest = mat_alloc(2, 2);
-    mat_dot(dest, unit_mat, b);
-    MAT_PRINT(dest);
+    Mat to = {
+        .rows = 4,
+        .cols = 1,
+        .stride = 3,
+        .es = td + 2,
+    };
 
-
+    Xor g = xor_init();
+    for (size_t i = 0; i < 100*1000; i++) {
+        xor_finite_diff(m, g, ti, to, eps);
+        xor_learn(m, g, rate);
+    }
+    printf("Cost = %f\n", xor_cost(m, ti, to));
+    xor_truth_taple(m);
     return 0;
 }
--- nn.h
+++ nn.h
@@ -1,11 +1,12 @@
 #ifndef NN_H_
 #define NN_H_
 
-#define MAT_AT(mat, i, j) (mat).data[(i) * (mat).cols + (j)]
+#define MAT_AT(mat, i, j) (mat).es[(i) * (mat).stride + (j)]
 #define MAT_PRINT(mat) mat_print(mat, #mat)
 
 #include <stddef.h>
 #include <stdio.h>
+#include <math.h>
 
 #ifndef NN_ASSERT
 #include <assert.h>
@@ -20,16 +21,21 @@
 typedef struct {
     size_t rows;
     size_t cols;
-    float* data;
+    size_t stride;
+    float* es;
 } Mat;
 
 float rand_float();
+float sigmoidf(float x);
 
 Mat mat_alloc(size_t rows, size_t cols);
-void mat_rand(Mat mat, float min, float max);
-void mat_fill(Mat mat, float x);
 void mat_dot(Mat des, Mat a, Mat b);
 void mat_sum(Mat des, Mat a);
+Mat mat_row(Mat mat, size_t row);
+void mat_copy(Mat dest, Mat src);
+void mat_rand(Mat mat, float min, float max);
+void mat_fill(Mat mat, float x);
+void mat_sig(Mat mat);
 void mat_print(Mat mat, const char* name);
 
 #endif // !NN_H_
@@ -39,30 +45,19 @@ float rand_float() {
     return (float)rand() / (float)RAND_MAX;
 }
 
+float sigmoidf(float x) {
+    return 1.f / (1.f + expf(-x));
+}
+
 Mat mat_alloc(size_t rows, size_t cols) {
     Mat m;
     m.rows = rows,
     m.cols = cols,
-    m.data = NN_MALLOC(sizeof(*m.data)*rows*cols);
+    m.stride = cols,
+    m.es = NN_MALLOC(sizeof(*m.es)*rows*cols);
 
-    NN_ASSERT(m.data != NULL);
+    NN_ASSERT(m.es != NULL);
     return m;
-}
-
-void mat_rand(Mat mat, float min, float max) {
-    for (size_t i = 0; i < mat.rows; i++) {
-        for (size_t j = 0; j < mat.cols; j++) {
-            MAT_AT(mat, i, j) = rand_float() * (max - min) + min;
-        }
-    }
-}
-
-void mat_fill(Mat mat, float x) {
-    for (size_t i = 0; i < mat.rows; i++) {
-        for (size_t j = 0; j < mat.cols; j++) {
-            MAT_AT(mat, i, j) = x;
-        }
-    }
 }
 
 void mat_dot(Mat des, Mat a, Mat b) {
@@ -88,6 +83,50 @@ void mat_sum(Mat des, Mat a) {
     for (size_t i = 0; i < des.rows; i++) {
         for (size_t j = 0; j < des.cols; j++) {
             MAT_AT(des, i, j) += MAT_AT(a, i, j);
+        }
+    }
+}
+
+Mat mat_row(Mat mat, size_t row) {
+    return (Mat) {
+        .rows = 1,
+        .cols = mat.cols,
+        .stride = mat.stride,
+        .es = &MAT_AT(mat, row, 0)
+    };
+}
+
+void mat_copy(Mat dest, Mat src) {
+    NN_ASSERT(dest.rows == src.rows);
+    NN_ASSERT(dest.cols == src.cols);
+
+    for (size_t i = 0; i < dest.rows; i++) {
+        for (size_t j = 0; j < dest.cols; j++) {
+            MAT_AT(dest, i, j) = MAT_AT(src, i, j);
+        }
+    }
+}
+
+void mat_rand(Mat mat, float min, float max) {
+    for (size_t i = 0; i < mat.rows; i++) {
+        for (size_t j = 0; j < mat.cols; j++) {
+            MAT_AT(mat, i, j) = rand_float() * (max - min) + min;
+        }
+    }
+}
+
+void mat_fill(Mat mat, float x) {
+    for (size_t i = 0; i < mat.rows; i++) {
+        for (size_t j = 0; j < mat.cols; j++) {
+            MAT_AT(mat, i, j) = x;
+        }
+    }
+}
+
+void mat_sig(Mat mat) {
+    for (size_t i = 0; i < mat.rows; i++) {
+        for (size_t j = 0; j < mat.cols; j++) {
+            MAT_AT(mat, i, j) = sigmoidf(MAT_AT(mat, i, j));
         }
     }
 }
