79e488c2d0622a5450dc409eeb043addf0f776b1
Mahmoud Elkholy <midoelkholy03@gmail.com>
2026-06-19 07:04:06 +0300

    Adding control kbd to the gym and make it improve UI

--- gym.c
+++ gym.c
@@ -10,9 +10,11 @@
 #include "nn.h"
 
 #define DA_INIT_CAPACITY 256
-#define IMG_FACTOR 80
-#define IMG_WIDTH  (16*IMG_FACTOR)
-#define IMG_HEIGHT (9*IMG_FACTOR)
+#define WINDOW_FACTOR 80
+#define WINDOW_WIDTH  (16*WINDOW_FACTOR)
+#define WINDOW_HEIGHT (9*WINDOW_FACTOR)
+#define BORDER_VPAD 50
+#define BORDER_HPAD 50
 
 #define DA_APPEND(da, item)                                                            \
     do {                                                                               \
@@ -38,16 +40,12 @@ typedef struct {
 
 void nn_render(NN nn, int x, int y, int w, int h)
 {
-    unsigned int neuron_raduis = h*((float)25/IMG_HEIGHT);
+    unsigned int neuron_raduis = h*((float)25/WINDOW_HEIGHT);
     Color low__color       = {.a =0xFF, .b =0xFF, .g =0x00, .r =0xFF};
     Color high_color       = {.a =0xFF, .b =0x00, .g =0xFF, .r =0x00};
-    
 
-    size_t border_vpad = 50;
-    size_t border_hpad = 50;
-
-    size_t nn_x = w - 2*border_hpad;
-    size_t nn_y = h - 2*border_vpad;
+    size_t nn_x = w - 2*BORDER_HPAD;
+    size_t nn_y = h - 2*BORDER_VPAD;
 
     size_t no_of_layers = nn.count + 1;
     int hpad = nn_x/no_of_layers;
@@ -55,16 +53,16 @@ void nn_render(NN nn, int x, int y, int w, int h)
     for (size_t i = 0; i < no_of_layers; i++) {
         int vpad_1 = nn_y/nn.as[i].cols;
         for (size_t j = 0; j < nn.as[i].cols; j++) {
-            int cx1 = x + border_hpad + i*hpad + hpad/2;
-            int cy1 = y +border_vpad + j*vpad_1 + vpad_1/2;
+            int cx1 = x + BORDER_HPAD + i*hpad + hpad/2;
+            int cy1 = y +BORDER_VPAD + j*vpad_1 + vpad_1/2;
             if (i < (no_of_layers - 1)) {
                 int vpad_2 = nn_y/nn.as[i+1].cols;
                 for (size_t k = 0; k < nn.as[i+1].cols; k++) {
-                    int cx2 = x + border_hpad + (i+1)*hpad + hpad/2;
-                    int cy2 = y +border_vpad + k*vpad_2 + vpad_2/2;
+                    int cx2 = x + BORDER_HPAD + (i+1)*hpad + hpad/2;
+                    int cy2 = y +BORDER_VPAD + k*vpad_2 + vpad_2/2;
                     
                     float alpha = sigmoidf(MAT_AT(nn.ws[i], j, k));;
-                    float thick = h*(3.0f/IMG_HEIGHT);
+                    float thick = h*(3.0f/WINDOW_HEIGHT);
                     Color connection_color = ColorAlphaBlend(low__color, high_color, WHITE);
                     high_color.a = floorf(255.f*alpha);
                     DrawLineEx((Vector2){cx1, cy1}, (Vector2){cx2, cy2}, thick, connection_color);
@@ -96,27 +94,50 @@ void get_cost_minmax(Costs costs, float* min, float* max)
 
 void cost_render(Costs costs, int x, int y, int w, int h)
 {
-    size_t boarder_hpad = 50;
-    size_t boarder_vpad = 50;
+    char buffer[256];
+    float font_size = h*(50.0f/WINDOW_HEIGHT);
 
-    w -= boarder_hpad;
-    h -= boarder_vpad;
+    snprintf(buffer, sizeof(buffer), "COST-FUNCTION");
+    int tw =  MeasureText(buffer, font_size);
+    DrawText(buffer, x+(w/2-tw/2), y+h, font_size, WHITE);
+
+    w -= BORDER_HPAD;
+    h -= BORDER_VPAD;
 
     float min, max;
     get_cost_minmax(costs, &min, &max);
     size_t n = costs.count;
 
     if (min > 0) min = 0;
+    /* TODO: MAKE THE PLOT MORE STABLE BUT THERE IS STILL A BUG */
     /* if (n < 1000) n = 1000; */
 
+    // DRAW THE AXIS LINES
+    int thick = h*(5.0f/WINDOW_HEIGHT);
+    int lx = x+BORDER_HPAD/2-thick;
+    int ly = y+BORDER_HPAD/2-thick;
+    float tri_val = (h*10.f/WINDOW_HEIGHT);
+
+    // X-AXIS
+    DrawLineEx((Vector2) {lx, ly+h}, (Vector2) {lx+w, ly+h}, thick, WHITE);
+    DrawTriangle((Vector2) {lx+w, ly+h-tri_val}, (Vector2) {lx+w, ly+h+tri_val}, (Vector2) {lx+w+tri_val, ly+h}, WHITE);
+
+    // Y-AXIS
+    DrawLineEx((Vector2) {lx, ly}, (Vector2) {lx, ly+h}, thick, WHITE);
+    DrawTriangle((Vector2) {lx-tri_val, ly}, (Vector2) {lx+tri_val, ly}, (Vector2) {lx, ly-tri_val}, WHITE);
+
+    snprintf(buffer, sizeof(buffer), "0");
+    DrawText(buffer, lx+5, ly+h+5, font_size, WHITE);
+
+
     for (size_t i = 0; (i+1) < n; i++) {
-        int x1 = x + boarder_hpad/2 + (float)w/n*i;
-        int y1 = y + boarder_vpad/2 + (1 - (costs.items[i]-min) / (max-min))*h;
+        int x1 = x + BORDER_HPAD/2 + (float)w/n*i;
+        int y1 = y + BORDER_VPAD/2 + (1 - (costs.items[i]-min) / (max-min))*h;
 
-        int x2 = x + boarder_hpad/2 + (float)w/n*(i+1);
-        int y2 = y + boarder_vpad/2 + (1 - (costs.items[i+1]-min) / (max-min))*h;
+        int x2 = x + BORDER_HPAD/2 + (float)w/n*(i+1);
+        int y2 = y + BORDER_VPAD/2 + (1 - (costs.items[i+1]-min) / (max-min))*h;
 
-        float thick = h*(5.0f/IMG_HEIGHT);
+        float thick = h*(5.0f/WINDOW_HEIGHT);
         DrawLineEx((Vector2) {x1, y1}, (Vector2) {x2, y2}, thick, RED);
     }
 }
@@ -203,13 +224,16 @@ int main(int argc, char** argv)
         .es     = &MAT_AT(t, 0, arch.items[0]),
     };
 
+
     // MODEL LEARNING
     NN nn = nn_alloc(arch.items, arch.count);
     NN g  = nn_alloc(arch.items, arch.count);
     nn_rand(nn, 0, 1);
 
+
+
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(IMG_WIDTH, IMG_HEIGHT, "GYM");
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "GYM");
     SetTargetFPS(60);
 
     srand(time(0));
@@ -217,13 +241,22 @@ int main(int argc, char** argv)
     size_t epoch = 0;
     size_t max_epoch = 5000;
     Costs costs = {0};
+
+    bool paused = true;
     while (!WindowShouldClose()) {
-        for (size_t j = 0; j < 7 && epoch < max_epoch; j++) {
+        if(IsKeyPressed(KEY_R)) {
+            epoch = 0;
+            nn_rand(nn, 0, 1);
+            costs.count = 0;
+        }
+        if (IsKeyPressed(KEY_SPACE)) paused = !paused;
+        for (size_t j = 0; j < 7 && epoch < max_epoch && !paused; j++) {
             nn_backprop(nn, g, ti, to);
             nn_learn(nn, g, rate);
             DA_APPEND(&costs, nn_cost(nn, ti, to));
             epoch++;
-        }
+        }    
+        
 
         Color background_color = {.a =0xFF, .b =0x18, .g =0x18, .r =0x18};
         BeginDrawing();
@@ -242,16 +275,30 @@ int main(int argc, char** argv)
         x = 0;
         cost_render(costs, x, y, w, h);
 
-        h = rh;
+
         char buffer[256];
-        float font_size = h*(30.0f/IMG_HEIGHT);
-        snprintf(buffer, sizeof(buffer), "Epoch: %zu/%zu\t\tRate: %.2f\t\tCost: %f", epoch, max_epoch, rate, nn_cost(nn, ti, to));
+        snprintf(buffer, sizeof(buffer),
+                 "Epoch: %zu/%zu\t\tRate: %.2f\t\tCost: %f",
+                 epoch, max_epoch, rate, nn_cost(nn, ti, to));
         
+        float font_size = h*(50.0f/WINDOW_HEIGHT);
         int tw =  MeasureText(buffer, font_size);
         DrawText(buffer, rw/2-tw/2, 30, font_size, WHITE);
+
         EndDrawing();
     }
 
+    // SAVING THE FINAL SNAPSHOT OF WINDOW TO THE SYSTEM AS PNG IMAGE
+    const char* img_file_name = "adder_nn.png";
+    Image final_img = LoadImageFromScreen();
+
+    if(!ExportImage(final_img, img_file_name)) {
+        fprintf(stderr, "ERROR: Couldn't save %s to the system\n", img_file_name);
+        return 1;
+    }
+
+    printf("Genereted image: %s\n", img_file_name);
     CloseWindow();
+
     return 0;
 }
