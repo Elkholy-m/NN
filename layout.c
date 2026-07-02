accdd4e413e613ac74be7ba079b1734dc03a1a6d
Mahmoud Elkholy <midoelkholy03@gmail.com>
2026-07-02 23:46:42 +0300

    Adding gaps to the layout

--- layout.c
+++ layout.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <raylib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define DA_INIT_CAPACITY 256
 #define DA_APPEND(da, item)                                                            \
@@ -39,11 +40,12 @@ typedef struct {
     LayoutOrient orient;
     size_t i;
     size_t count;
+    size_t gap;
 } Layout;
 
-Layout layout_constructor(LayoutRect rect, LayoutOrient orient, size_t count)
+Layout layout_constructor(LayoutRect rect, LayoutOrient orient, size_t count, size_t gap)
 {
-    return (Layout) {.rect = rect, .orient = orient, .count = count};
+    return (Layout) {.rect = rect, .orient = orient, .count = count, .gap = gap};
 }
 
 typedef struct {
@@ -52,8 +54,9 @@ typedef struct {
     size_t capacity;
 } LayoutStack;
 
-void layout_stack_push(LayoutStack* ls, Layout l)
+void layout_stack_push(LayoutStack* ls, LayoutRect rect, LayoutOrient orient, size_t count, size_t gap)
 {
+    Layout l = layout_constructor(rect, orient, count, gap);
     DA_APPEND(ls, l);
 }
 
@@ -64,8 +67,13 @@ void layout_stack_pop(LayoutStack* ls)
 }
 
 
-LayoutRect layout_slot(Layout *l)
+LayoutRect layout_slot(Layout *l, const char* file, const int line)
 {
+    if (l->i >= l->count) {
+        fprintf(stderr, "%s:%d:\tERROR: Widget overfloaw\n", file, line);
+        exit(EXIT_FAILURE);
+    }
+
     LayoutRect r = {0};
     switch(l->orient) {
     case LO_HORZ:
@@ -73,12 +81,32 @@ LayoutRect layout_slot(Layout *l)
         r.h = l->rect.h;
         r.x = l->rect.x+l->i*r.w;
         r.y = l->rect.y;
+
+        if (l->i == 0) { // first
+            r.w -= l->gap/2;
+        } else if (l->i == l->count-1) { // last
+            r.x += l->gap/2;
+            r.w -= l->gap/2;
+        } else { //middle
+            r.x += l->gap/2;
+            r.w -= l->gap;
+        }
         break;
     case LO_VERT:
         r.w = l->rect.w;
         r.h = l->rect.h/l->count;
         r.x = l->rect.x;
         r.y = l->rect.y+l->i*r.h;
+
+        if (l->i == 0) { // first
+            r.h -= l->gap/2;
+        } else if (l->i == l->count-1) { // last
+            r.y += l->gap/2;
+            r.h -= l->gap/2;
+        } else { //middle
+            r.y += l->gap/2;
+            r.h -= l->gap;
+        }
         break;
     default:
         assert(0 && "Unreachable");
@@ -88,15 +116,29 @@ LayoutRect layout_slot(Layout *l)
     return r;
 }
 
-LayoutRect layout_stack_slot(LayoutStack* ls)
+LayoutRect layout_stack_slot_imp(LayoutStack* ls, const char* file, const int line)
 {
     assert(ls->count > 0);
-    return layout_slot(&ls->items[ls->count-1]);
+    return layout_slot(&ls->items[ls->count-1], file, line);
 }
+
+#define layout_stack_slot(ls) layout_stack_slot_imp(ls, __FILE__, __LINE__)
+
 
 void widget(LayoutRect rect, Color c)
 {
-    DrawRectangle(rect.x, rect.y, rect.w, rect.h, c);
+    Rectangle rec = {
+        .x = rect.x,
+        .y = rect.y,
+        .width = rect.w,
+        .height = rect.h,
+    };
+
+    if(CheckCollisionPointRec(GetMousePosition(), rec)) {
+        c = ColorBrightness(c, 0.65);
+    }
+
+    DrawRectangleRec(rec,  c);
 }
 
 int main(void)
@@ -111,13 +153,28 @@ int main(void)
         ClearBackground(BLACK);
         int w = GetRenderWidth();
         int h = GetRenderHeight();
-        layout_stack_push(&ls, layout_constructor(rect_constructor(0, 0, w, h), LO_HORZ, 3));
+        size_t frame = h*0.1;
+        size_t gap = h*0.015;
+        layout_stack_push(&ls, rect_constructor(0, frame, w, h-2*frame), LO_HORZ, 3, gap);
             widget(layout_stack_slot(&ls), BLUE);
             widget(layout_stack_slot(&ls), GREEN);
-                layout_stack_push(&ls, layout_constructor(layout_stack_slot(&ls), LO_VERT, 3));
+            layout_stack_push(&ls, layout_stack_slot(&ls), LO_VERT, 3, gap);
+                layout_stack_push(&ls, layout_stack_slot(&ls), LO_HORZ, 2, gap);
+                    layout_stack_push(&ls, layout_stack_slot(&ls), LO_VERT, 2, gap);
+                    widget(layout_stack_slot(&ls), SKYBLUE);
+                        layout_stack_push(&ls, layout_stack_slot(&ls), LO_HORZ, 2, gap);
+                        widget(layout_stack_slot(&ls), SKYBLUE);
+                        widget(layout_stack_slot(&ls), SKYBLUE);
+                        layout_stack_pop(&ls);
+                    layout_stack_pop(&ls);
                 widget(layout_stack_slot(&ls), RED);
-                widget(layout_stack_slot(&ls), WHITE);
-                widget(layout_stack_slot(&ls), BLACK);
+                layout_stack_pop(&ls);
+                layout_stack_push(&ls, layout_stack_slot(&ls), LO_HORZ, 3, gap);
+                widget(layout_stack_slot(&ls), YELLOW);
+                widget(layout_stack_slot(&ls), YELLOW);
+                widget(layout_stack_slot(&ls), YELLOW);
+                layout_stack_pop(&ls);
+                widget(layout_stack_slot(&ls), MAGENTA);
                 layout_stack_pop(&ls);
             layout_stack_pop(&ls);
         EndDrawing();
